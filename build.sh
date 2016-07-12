28b21031f237a19e423c994e094c2009f34ee54d
Radek Dymacz <radekdymacz@gmail.com>
2016-07-12 16:55:39 +0100

    ui services

--- build.sh
+++ build.sh
@@ -5,13 +5,13 @@ set -x
 
 REGISTRY=kazoup
 
+
 # Used to rebuild all the things
 
 find * -type d -maxdepth 1 -print | while read dir; do
 	if [ ! -f $dir/Dockerfile ]; then
 		continue
 	fi
-
 
 	pushd $dir >/dev/null
 
@@ -26,6 +26,19 @@ find * -type d -maxdepth 1 -print | while read dir; do
 
 	# build static binary
 	CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo  .
+	
+	# build ui  		
+	if [[ ${dir%/*} = "ui" && ${dir#*/} != "static" ]]; then
+		mkdir -p frontend/dist/sections/${dir#*/}
+		cp -R -f ../frontend/dist/sections/${dir#*/} frontend/dist/sections
+		IMAGE=${dir#*/}-web
+	fi
+
+	if [[ ${dir%/*} = "ui" && ${dir#*/} = "static" ]]; then
+		mkdir -p frontend
+		cp -R -f ../frontend/dist frontend
+		IMAGE=${dir#*/}-web
+	fi
 
 	# build docker image
 	docker build -t $REGISTRY/$IMAGE .
