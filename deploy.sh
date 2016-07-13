a4694c0b0fa73f185e156f8f65fda9138fb0f95a
Radek Dymacz <radekdymacz@gmail.com>
2016-07-13 09:14:15 +0100

    fix build

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ REGISTRY=kazoup
 
 # Build UI
 
-cd ui/frontend && npm install && npm install gulp && bower install && gulp && cd ../..
+cd ui/frontend && npm install && npm install gulp && bower install && node_modules/gulp/bin/gulp.js && cd ../..
 
 # Used to rebuild all the things
 
