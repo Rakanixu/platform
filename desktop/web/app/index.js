83ba9a21f84e38f3c8f59f2ce1604f41efe7feb4
Radek Dymacz <radekdymacz@gmail.com>
2016-07-29 16:23:36 +0100

    [ci skip] WIP detect disks with Node

--- desktop/web/app/index.js
+++ desktop/web/app/index.js
@@ -1,43 +1,34 @@
 // In renderer process (web page).
 const electron = require('electron');
-const {ipcRenderer} = electron;
+const {
+    ipcRenderer
+} = electron;
 
 
 function boot() {
 
     console.log("DOM fully loaded and parsed");
-// check for Geolocation support
-	if (navigator.geolocation) {
-  		console.log('Geolocation is supported!');
-	}
-	else {
-	  console.log('Geolocation is not supported for this Browser/OS version yet.');
-	}
-	var startPos;
-  var geoSuccess = function(position) {
-    	startPos = position;
-	console.log(position);
-    	//document.getElementById('startLat').innerHTML = startPos.coords.latitude;
-    	//document.getElementById('startLon').innerHTML = startPos.coords.longitude;
-  	};
- 	 navigator.geolocation.getCurrentPosition(geoSuccess);
-    ipcRenderer.send("starting","elasticsearch");
+    // check for Geolocation support
+    if (navigator.geolocation) {
+        console.log('Geolocation is supported!');
+    } else {
+        console.log('Geolocation is not supported for this Browser/OS version yet.');
+    }
+    var startPos;
+    var geoSuccess = function(position) {
+        startPos = position;
+        console.log(position);
+    };
+    navigator.geolocation.getCurrentPosition(geoSuccess);
+    ipcRenderer.send("starting", "elasticsearch");
+    ipcRenderer.send("disks-message");
 }
 
 document.addEventListener('DOMContentLoaded', boot);
-//document.addEventListener("DOMContentLoaded", function(event){
-//    var start = window.document.getElementById("start")
-//	start.addEventListener('click',function(){
-//		console.log("Start services")
-//		ipcRenderer.send("starting","elasticsearch")
-//	});
-//
-//
-//});
 
 ipcRenderer.on('update-message', function(event, method) {
     alert(method);
 });
-ipcRenderer.on('disks-message',function(event,disks){
-   console.log("Got disks",disks);
+ipcRenderer.on('disks-message', function(event, disks) {
+    console.log("Got disks", disks);
 });
