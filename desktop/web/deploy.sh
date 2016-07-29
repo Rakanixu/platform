ee7c712cb86e9d69da46f7c3c58035ec438d0a20
Radek Dymacz <radekdymacz@gmail.com>
2016-07-29 16:38:48 +0100

    [ci skip] bumped version to 0.5

--- desktop/web/deploy.sh
+++ desktop/web/deploy.sh
@@ -6,4 +6,4 @@ USERNAME=kazoup
 REPONAME=platform
 WORKING_DIR=$PWD
 go get github.com/tcnksm/ghr
-ghr -t $GITHUB_TOKEN -u $USERNAME -r $REPONAME  --replace v0.0.4 release/
+ghr -t $GITHUB_TOKEN -u $USERNAME -r $REPONAME  --replace v0.0.5 release/
