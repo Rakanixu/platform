8f4b434d2da31ae5ac988f94129aec2107674159
Radek Dymacz <radekdymacz@gmail.com>
2016-08-04 16:25:44 +0100

    [ci skip] WIP

--- desktop/web/handler/webm.go
+++ desktop/web/handler/webm.go
@@ -22,9 +22,26 @@ func (s *WebmHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	//idx, _ := strconv.ParseInt(r.URL.Path[strings.LastIndex(r.URL.Path, "/")+1:strings.LastIndex(r.URL.Path, ".")], 0, 64)
 	//:startTime := 0
 	w.Header()["Content-Type"] = []string{"video/webm"}
-
+	w.Header()["X-Content-Duration"] = []string{"5"}
+	w.Header()["Content-Duration"] = []string{"750"}
+	w.Header()["Accept-Ranges"] = []string{"bytes"}
+	w.Header().Set("Transfer-Encoding", "identity")
 	log.Printf("Streaming %v", filePath)
-	cmd := exec.Command("/Users/radekdymacz/Downloads/ffmpeg", "-i", filePath, "-ss", "0", "-t", "5", "-c:v", "libvpx", "-profile:v", "baseline", "-b:v", "2000k", "-c:a", "libvorbis", "-an", "-f", "webm", "-dash", "1", "-")
-
+	cmd := exec.Command(
+		"/Users/radekdymacz/Downloads/ffmpeg",
+		"-ss", "0", //starting time offset
+		"-t", "5",
+		"-c:v", "libvpx", //video using vpx (webm) codec
+		"-b:v", "1M", //1Mb/s bitrate for the video
+		"-crf", "10",
+		"-cpu-used", "2", //total # of cpus used
+		"-threads", "4", //number of threads shared between all cpu-used
+		"-deadline", "realtime", //speeds up transcode time (necessary unless you want frames dropped)
+		"-strict", "-2", //ffmpeg complains about using vorbis, and wanted this param
+		"-c:a", "libvorbis", //audio using the vorbis (ogg) codec
+		"-s", "1280x800", //size
+		"-f", "webm", //filetype for the pipe
+		"-", //send output to stdout
+	)
 	ServeCommand(cmd, w)
 }
