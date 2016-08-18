b55952929a0ba25a49665d5a62bc63bff73d0674
Rakanixu <paguirrerubio@gmail.com>
2016-08-18 09:30:48 +0100

    [CI skip] WIP

--- db/srv/engine/bleve/helper.go
+++ db/srv/engine/bleve/helper.go
@@ -15,6 +15,30 @@ func (b *bleve) walkHandler() filepath.WalkFunc {
 		default:
 			log.Println(path)
 
+			err := errors.New("")
+			b.mu.Lock()
+
+			
+			b.indexMap[path], err = lib.Open(path)
+			if err != nil {
+		/*	mapping := lib.NewIndexMapping()
+			mapping.StoreDynamic = true
+
+			documentMapping := lib.NewTextFieldMapping()
+			documentMapping.Store = true
+
+			mapping.DefaultMapping.AddFieldMappingsAt("data", documentMapping)*/
+			b.idx, err = lib.New(path, lib.NewIndexMapping())
+			if err != nil {
+				log.Fatalf("Error creating index : %s", err.Error())
+				return err
+			}
+
+			b.mu.Unlock()
+			return nil
+		}
+
+
 		}
 
 		return nil
