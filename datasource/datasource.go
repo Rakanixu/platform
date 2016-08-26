fdcb0ed252a7b8bfa059009b109e1ef366283894
Radek Dymacz <radekdymacz@gmail.com>
2016-08-26 09:23:41 +0100

    cleanup

--- datasource/datasource.go
+++ datasource/datasource.go
@@ -4,27 +4,10 @@ import (
 	"log"
 	"time"
 
-	datasource_api "github.com/kazoup/platform/datasource/api/handler"
 	datasource_srv "github.com/kazoup/platform/datasource/srv/handler"
 	"github.com/micro/cli"
 	"github.com/micro/go-micro"
 )
-
-func api(ctx *cli.Context) {
-
-	service := micro.NewService(
-		micro.Name("go.micro.api.datasource"),
-		micro.RegisterTTL(time.Minute),
-		micro.RegisterInterval(time.Second*30),
-	)
-
-	service.Server().Handle(
-		service.Server().NewHandler(new(datasource_api.Datasource)),
-	)
-	if err := service.Run(); err != nil {
-		log.Fatalf("%v", err)
-	}
-}
 
 func srv(ctx *cli.Context) {
 
@@ -42,16 +25,12 @@ func srv(ctx *cli.Context) {
 	}
 }
 func datasourceCommands() []cli.Command {
-	return []cli.Command{{
-		Name:   "api",
-		Usage:  "Run datasource api service",
-		Action: api,
-	}, {
-
-		Name:   "srv",
-		Usage:  "Run datasource srv service",
-		Action: srv,
-	},
+	return []cli.Command{
+		{
+			Name:   "srv",
+			Usage:  "Run datasource srv service",
+			Action: srv,
+		},
 	}
 }
 func Commands() []cli.Command {
