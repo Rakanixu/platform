aad4a65441c4149976412ca4b73fbefb8d352755
Radek Dymacz <radekdymacz@gmail.com>
2016-08-26 17:07:58 +0100

    switch to single executable with desktop  version

---
+++ cmd/kazoup/main.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	auth "github.com/kazoup/platform/auth"
+	config "github.com/kazoup/platform/config"
+	crawler "github.com/kazoup/platform/crawler"
+	datasource "github.com/kazoup/platform/datasource"
+	elastic "github.com/kazoup/platform/elastic"
+	"github.com/micro/micro/web"
+	"github.com/micro/cli"
+	ccli "github.com/micro/cli"
+	"github.com/micro/go-micro/cmd"
+	"log"
+	"os"
+	"os/exec"
+	"sync"
+)
+
+func main() {
+	app := cmd.App()
+	app.Commands = append(app.Commands, auth.Commands()...)
+	app.Commands = append(app.Commands, config.Commands()...)
+	app.Commands = append(app.Commands, crawler.Commands()...)
+	app.Commands = append(app.Commands, datasource.Commands()...)
+	app.Commands = append(app.Commands, elastic.Commands()...)
+	app.Commands = append(app.Commands, web.Commands()...)
+	app.Commands = append(app.Commands, desktopCommands()...)
+	app.Action = func(context *cli.Context) { cli.ShowAppHelp(context) }
+	setup(app)
+	cmd.Init(
+		cmd.Name("kazoup"),
+		cmd.Description("Kazoup platform"),
+		cmd.Version("latest"),
+	)
+}
+
+func setup(app *ccli.App) {
+	// common flags
+	app.Flags = append(app.Flags,
+		ccli.IntFlag{
+			Name:   "register_ttl",
+			EnvVar: "MICRO_REGISTER_TTL",
+			Usage:  "Register TTL in seconds",
+		},
+		ccli.IntFlag{
+			Name:   "register_interval",
+			EnvVar: "MICRO_REGISTER_INTERVAL",
+			Usage:  "Register interval in seconds",
+		},
+		ccli.StringFlag{
+			Name:   "html_dir",
+			EnvVar: "MICRO_HTML_DIR",
+			Usage:  "The html directory for a web app",
+		},
+		ccli.StringFlag{
+			Name:   "elasticsearch_hosts",
+			EnvVar: "ELASTICSEARCH_HOSTS",
+			Usage:  "ELasticsearch hosts ie: localhost:9200",
+		},
+	)
+}
+
+func desktop(ctx *ccli.Context) {
+	var wg sync.WaitGroup
+	cmds := ctx.App.Commands
+
+	log.Print(ctx.Args())
+	for _,cmd := range cmds {
+		if cmd.Name != "help" && len(cmd.Subcommands) > 0 {
+			for _,subcmd := range cmd.Subcommands { 
+			//time.Sleep(time.Second)
+			wg.Add(1)
+			log.Print(cmd.Name,subcmd.Name)
+			c := exec.Command("./kazoup","--registry=mdns",cmd.Name,subcmd.Name)
+			c.Stdout = os.Stdout
+			c.Stderr = os.Stderr
+			if err := c.Start();err != nil{
+				log.Print(err.Error())
+				wg.Done()
+			}
+			}
+		}
+		if cmd.Name != "help" && len(cmd.Subcommands) == 0 && cmd.Name != "desktop"{
+
+			wg.Add(1)
+			log.Print(cmd.Name)
+			c := exec.Command("./kazoup","--registry=mdns",cmd.Name)
+			c.Stdout = os.Stdout
+			c.Stderr = os.Stderr
+			if err := c.Start();err != nil{
+				log.Print(err.Error())
+				wg.Done()
+			}
+		}
+	}
+	wg.Wait()
+
+}
+
+
+func desktopCommands() []cli.Command {
+	return []cli.Command{{
+		Name:   "desktop",
+		Usage:  "Run desktop service",
+		Action: desktop,
+	},
+	}
+}
+func Commands() []cli.Command {
+	return []cli.Command{
+		{
+			Name:        "desktop",
+			Usage:       "Auth commands",
+			Subcommands: desktopCommands(),
+		},
+	}
+}
