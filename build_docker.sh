afc090537e7d398c6d72b80ff3152745f7fd932b
Radek Dymacz <radekdymacz@gmail.com>
2016-08-26 13:55:05 +0100

    fix docker builds

--- build_docker.sh
+++ build_docker.sh
@@ -5,9 +5,6 @@ set -x
 
 REGISTRY=kazoup
 
-# Build UI
-
-cd ui/frontend && npm install && npm install gulp && bower install && node_modules/gulp/bin/gulp.js && cd ../..
 
 # Used to rebuild all the things
 
@@ -30,18 +27,6 @@ find * -type d -maxdepth 1 -print | while read dir; do
 	# build static binary
 	CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo  .
 	
-	# build ui  		
-	if [[ ${dir%/*} = "ui" && ${dir#*/} != "static" ]]; then
-		mkdir -p frontend/dist/sections/${dir#*/}
-		cp -R -f ../frontend/dist/sections/${dir#*/} frontend/dist/sections
-		IMAGE=${dir#*/}-web
-	fi
-
-	if [[ ${dir%/*} = "ui" && ${dir#*/} = "static" ]]; then
-		mkdir -p frontend
-		cp -R -f ../frontend/dist frontend
-		IMAGE=${dir#*/}-web
-	fi
 
 	# build docker image
 	docker build -t $REGISTRY/$IMAGE .
--- desktop/web/build-bin.sh
+++ desktop/web/build-bin.sh
@@ -9,10 +9,11 @@ go get github.com/micro/micro
 WORKING_DIR=$PWD
 DESKTOP_DIR=$GOPATH/src/github.com/kazoup/platform/desktop
 # add ES download to build on circle
-# https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/zip/elasticsearch/2.3.4/elasticsearch-2.3.4.zip
+cd $DESKTOP_DIR/web && rm -rf elasticsearch && wget https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/zip/elasticsearch/2.3.4/elasticsearch-2.3.4.zip 
+unzip elasticsearch-2.3.4.zip && rm -rf elasticsearch-2.3.4.zip && mv elasticsearch-2.3.4 elasticsearch
 
 #cd ../../../micro/micro && gox -verbose -os="darwin linux" -arch="386 amd64" -output $WORKING_DIR/bin/micro/{{.OS}}/{{.Arch}}/micro
-#cd $WORKING_DIR
+cd $WORKING_DIR
 
 #cd ../ui/frontend && npm install && npm install gulp && bower install && node_modules/gulp/bin/gulp.js && cd ../..
 
@@ -26,9 +27,9 @@ gox -verbose -os="darwin linux windows" -arch="amd64" -output $DESKTOP_DIR/web/b
 cd $GOPATH/src/github.com/micro/micro
 gox -verbose -os="darwin linux windows" -arch="amd64" -output $DESKTOP_DIR/web/bin/{{.OS}}/{{.Arch}}/micro
 cd $WORKING_DIR
-#cd $DESKTOP_DIR/web/bin/darwin/amd64 && wget http://evermeet.cx/ffmpeg/ffmpeg-3.1.2.7z && 7z x ffmpeg-3.1.2.7z && rm -rf ffmpeg-3.1.2.7z
-#cd $DESKTOP_DIR/web/bin/darwin/amd64 && wget http://evermeet.cx/ffmpeg/ffprobe-3.1.2.7z && 7z x ffprobe-3.1.2.7z && rm -rf ffprobe-3.1.2.7z
-#cd $DESKTOP_DIR/web/bin/linux/amd64 && wget http://johnvansickle.com/ffmpeg/releases/ffmpeg-release-32bit-static.tar.xz && tar -xJf  ffmpeg-release-32bit-static.tar.xz && rm -rf ffmpeg-release-32bit-static.tar.xz
-#cd $DESKTOP_DIR/web/bin/windows/amd64 && wget https://ffmpeg.zeranoe.com/builds/win64/static/ffmpeg-3.0.1-win64-static.7z && 7z x ffmpeg-3.0.1-win64-static.7z && rm -rf ffmpeg-3.0.1-win64-static.7z
+cd $DESKTOP_DIR/web/bin/darwin/amd64 && wget http://evermeet.cx/ffmpeg/ffmpeg-3.1.2.7z && 7z x ffmpeg-3.1.2.7z && rm -rf ffmpeg-3.1.2.7z
+cd $DESKTOP_DIR/web/bin/darwin/amd64 && wget http://evermeet.cx/ffmpeg/ffprobe-3.1.2.7z && 7z x ffprobe-3.1.2.7z && rm -rf ffprobe-3.1.2.7z
+cd $DESKTOP_DIR/web/bin/linux/amd64 && wget http://johnvansickle.com/ffmpeg/releases/ffmpeg-release-32bit-static.tar.xz && tar -xJf  ffmpeg-release-32bit-static.tar.xz && rm -rf ffmpeg-release-32bit-static.tar.xz
+cd $DESKTOP_DIR/web/bin/windows/amd64 && wget https://ffmpeg.zeranoe.com/builds/win64/static/ffmpeg-3.0.1-win64-static.7z && 7z x ffmpeg-3.0.1-win64-static.7z && rm -rf ffmpeg-3.0.1-win64-static.7z
 
 #cd $WORKING_DIR
