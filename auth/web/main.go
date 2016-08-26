590616482fc52c2ed1f700319a4c01b0512fd95e
Radek Dymacz <radekdymacz@gmail.com>
2016-08-26 17:07:58 +0100

    switch to single executable with desktop  version

--- auth/web/main.go
+++ auth/web/main.go
@@ -12,6 +12,7 @@ func main() {
 	service := web.NewService(web.Name("go.micro.web.auth"))
 	service.HandleFunc("/google/login", handler.HandleGoogleLogin)
 	service.HandleFunc("/GoogleCallback", handler.HandleGoogleCallback)
+	service.HandleFunc("/google/callback", handler.HandleGoogleCallback)
 	service.HandleFunc("/microsoft/login", handler.HandleMicrosoftLogin)
 	service.HandleFunc("/microsoft/callback", handler.HandleMicrosoftCallback)
 	service.HandleFunc("/slack/login", handler.HandleSlackLogin)
