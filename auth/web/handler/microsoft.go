c4cac7555b4457dffa8dbe45636aa61e4b39da8a
Pablo Aguirre <paguirrerubio@gmail.com>
2016-09-08 11:02:29 +0100

    [CI skip] onedrive username

--- auth/web/handler/microsoft.go
+++ auth/web/handler/microsoft.go
@@ -1,8 +1,10 @@
 package handler
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/kazoup/platform/structs/globals"
+	"github.com/kazoup/platform/structs/onedrive"
 	"golang.org/x/oauth2"
 	"log"
 	"net/http"
@@ -24,7 +26,33 @@ func HandleMicrosoftCallback(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	url := fmt.Sprintf("onedrive://%s", token.Extra("user_id"))
+	// Get user name
+	c := &http.Client{}
+	//https://api.onedrive.com/v1.0/drives
+	u := globals.OneDriveEndpoint + "drives"
+	req, err := http.NewRequest("GET", u, nil)
+	req.Header.Set("Authorization", token.TokenType+" "+token.AccessToken)
+	if err != nil {
+		log.Println(err)
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
+	res, err := c.Do(req)
+	if err != nil {
+		log.Println(err)
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
+	defer res.Body.Close()
+
+	var drivesRsp *onedrive.DrivesListResponse
+	if err := json.NewDecoder(res.Body).Decode(&drivesRsp); err != nil {
+		log.Println(err)
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
+
+	url := fmt.Sprintf("onedrive://%s", drivesRsp.Value[0].Owner.User.DisplayName)
 
 	if err := SaveDatasource(url, token); err != nil {
 		fmt.Fprintf(w, "Error adding data source %s \n", err.Error())
