96a927e5d218d6aa7168a2faba414c354942dfb5
Pablo Aguirre <paguirrerubio@gmail.com>
2016-09-26 14:11:25 +0100

    [CI skip] Previews

--- structs/file/kazoup_local_file.go
+++ structs/file/kazoup_local_file.go
@@ -10,14 +10,20 @@ type KazoupLocalFile struct {
 }
 
 func (kf *KazoupLocalFile) PreviewURL(width, height, mode, quality string) string {
-	url := fmt.Sprintf("%s/image/local?source=/%s&width=%s&height=%s&mode=%s&quality=%s",
-		BASE_URL_FILE_PREVIEW,
-		strings.TrimLeft(kf.URL, "/local"),
-		width,
-		height,
-		mode,
-		quality,
-	)
+	var url string
+
+	if kf.Category == "Movies" {
+		url = fmt.Sprintf("http://localhost:8082/media/frame/%s", strings.TrimLeft(kf.URL, "/local"))
+	} else {
+		url = fmt.Sprintf("%s/image/local?source=/%s&width=%s&height=%s&mode=%s&quality=%s",
+			BASE_URL_FILE_PREVIEW,
+			strings.TrimLeft(kf.URL, "/local"),
+			width,
+			height,
+			mode,
+			quality,
+		)
+	}
 
 	return url
 }
