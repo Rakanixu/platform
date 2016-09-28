d489499c7abf9d799040a98c98bd0dcf060bc6da
Pablo Aguirre <paguirrerubio@gmail.com>
2016-09-28 14:47:52 +0100

    [Ci skip] Fix databinding when addind datasources

--- desktop/web/app/windows.js
+++ desktop/web/app/windows.js
@@ -7,6 +7,7 @@ const {autoUpdater} = electron;
 // Keep a global reference of the window object, if you don"t, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win;
+let auth;
 
 module.exports = (function() {
   let updateFeed = "";
