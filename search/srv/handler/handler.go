eafc867764f19471224f39b5e5f4fb5c25b6dddb
Pablo Aguirre <paguirrerubio@gmail.com>
2016-09-28 15:32:01 +0100

    [CI skip] Add custom headers

--- search/srv/handler/handler.go
+++ search/srv/handler/handler.go
@@ -4,7 +4,10 @@ import (
 	"github.com/kazoup/platform/search/srv/engine"
 	proto "github.com/kazoup/platform/search/srv/proto/search"
 	"github.com/micro/go-micro/client"
+	//"github.com/micro/go-micro/errors"
+	//"github.com/micro/go-micro/metadata"
 	"golang.org/x/net/context"
+	//"log"
 )
 
 // Search struct
@@ -14,6 +17,14 @@ type Search struct {
 
 // Search srv handler
 func (s *Search) Search(ctx context.Context, req *proto.SearchRequest, rsp *proto.SearchResponse) error {
+	/*	md, ok := metadata.FromContext(ctx)
+		if !ok {
+			return errors.InternalServerError("search.search", "Unable to retrieve metadata")
+		}
+		log.Println("CONTEXT")
+		log.Println(md["User"])
+		log.Println(md["Token"])*/
+
 	response, err := engine.Search(ctx, req, s.Client)
 	if err != nil {
 		return err
