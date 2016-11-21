bf00b42bf0c4a5247047806db109726da1cc8a9e
Radek Dymacz <radekdymacz@gmail.com>
2016-11-21 14:46:59 +0000

    rolling updates first try

--- deploy-circle.sh
+++ deploy-circle.sh
@@ -20,7 +20,16 @@ find * -type d -maxdepth 1 -print | while read dir; do
  	
 	# push docker image
 	sudo /opt/google-cloud-sdk/bin/gcloud docker push eu.gcr.io/${PROJECT_NAME}/$IMAGE
-	
+	if [ $? -ne 0 ]; then
+   		 echo "Could not push the image"
+      exit 1;
+  	fi;
+	sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
+	kubectl set image deployment/$IMAGE $IMAGE=eu.gcr.io/desktop-1470249894548/$IMAGE:$CIRCLE_SHA1
+	if [ $? -ne 0 ]; then
+   		 echo "Could not perform rolling update"
+      exit 1;
+  	fi;
 
 	popd >/dev/null
 done
