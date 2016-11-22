44c3bf01f3751c229a76320a333a8e53b677e8e6
Radek Dymacz <radekdymacz@gmail.com>
2016-11-22 10:00:08 +0000

    refactor saving datasources using simple client

--- auth/web/handler/helper.go
+++ auth/web/handler/helper.go
@@ -1,13 +1,11 @@
 package handler
 
 import (
-	"fmt"
-	"log"
-
 	proto_datasource "github.com/kazoup/platform/datasource/srv/proto/datasource"
 	"github.com/kazoup/platform/lib/globals"
 	"github.com/kazoup/platform/lib/wrappers"
 	notification_proto "github.com/kazoup/platform/notification/srv/proto/notification"
+	"github.com/micro/go-micro/errors"
 	"golang.org/x/net/context"
 	"golang.org/x/oauth2"
 )
@@ -15,29 +13,34 @@ import (
 //SaveDatasource call datasource-srv and save new data source
 func SaveDatasource(ctx context.Context, user string, url string, token *oauth2.Token) error {
 
-	c := proto_datasource.NewDataSourceClient("com.kazoup.srv.datasource", wrappers.NewKazoupClient())
-	log.Print(c)
-	req := &proto_datasource.CreateRequest{
-		Endpoint: &proto_datasource.Endpoint{
-			UserId:          user,
-			Url:             url,
-			LastScan:        0,
-			LastScanStarted: 0,
-			CrawlerRunning:  false,
-			Token: &proto_datasource.Token{
-				AccessToken:  token.AccessToken,
-				TokenType:    token.TokenType,
-				RefreshToken: token.RefreshToken,
-				Expiry:       token.Expiry.Unix(),
+	client := wrappers.NewKazoupClient()
+
+	srvReq := client.NewRequest(
+		globals.DATASOURCE_SERVICE_NAME,
+		"DataSource.Crete",
+		&proto_datasource.CreateRequest{
+			Endpoint: &proto_datasource.Endpoint{
+				UserId:          user,
+				Url:             url,
+				LastScan:        0,
+				LastScanStarted: 0,
+				CrawlerRunning:  false,
+				Token: &proto_datasource.Token{
+					AccessToken:  token.AccessToken,
+					TokenType:    token.TokenType,
+					RefreshToken: token.RefreshToken,
+					Expiry:       token.Expiry.Unix(),
+				},
 			},
 		},
+	)
+
+	srvRes := &proto_datasource.CreateResponse{}
+
+	if err := client.Call(ctx, srvReq, srvRes); err != nil {
+		return errors.New("auth.web.helper.DataSource.Create", err.Error(), 500)
 	}
 
-	_, err := c.Create(ctx, req)
-	if err != nil {
-		fmt.Print(err)
-		return err
-	}
 	return nil
 }
 
