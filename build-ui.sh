bb78893e467fa01d906357566a3a5a3c83d9b800
Radek Dymacz <radekdymacz@gmail.com>
2016-11-23 22:45:53 +0000

    serve source files for now

--- build-ui.sh
+++ build-ui.sh
@@ -10,5 +10,5 @@ rm -rf  ui/web/html
 cd ui/src && npm install && bower install
 npm install polymer-cli
 node_modules/polymer-cli/bin/polymer.js build -v
-cd .. && cp -r src/build/bundled web/html
+cd .. && cp -r src web/html
 cd $CWD
