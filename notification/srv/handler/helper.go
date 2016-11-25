c1ff86aa3dc219f748db7f67bb34ebf86b0e938e
Pablo Aguirre <paguirrerubio@gmail.com>
2016-11-25 10:15:01 +0000

    Test

--- notification/srv/handler/helper.go
+++ notification/srv/handler/helper.go
@@ -13,7 +13,7 @@ func StreamNotifications(s server.Server, req *proto.StreamRequest) (chan *proto
 	exit := make(chan bool)
 
 	// We subscribe directly to the broker to be able to handle the data internally
-	sub, err := s.Options().Broker.Subscribe(globals.NotificationTopic, func(p broker.Publication) error {
+	_, err := s.Options().Broker.Subscribe(globals.NotificationTopic, func(p broker.Publication) error {
 		var e *proto.NotificationMessage
 
 		if err := json.Unmarshal(p.Message().Body, &e); err != nil {
