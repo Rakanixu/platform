cfb1b9a2c4492698c6bf8b5dc802cc9ebb17aba2
Pablo Aguirre <paguirrerubio@gmail.com>
2016-11-28 14:46:36 +0000

    Debug

--- notification/srv/handler/handler.go
+++ notification/srv/handler/handler.go
@@ -23,11 +23,14 @@ func (n *Notification) Stream(ctx context.Context, stream server.Streamer) error
 	// StreamNotifications subscribes to NotificationTopic and return channels for communications
 	ch, exit, err := StreamNotifications(n.Server, req)
 	if err != nil {
+		fmt.Println("ERROR StreamNotifications", err)
 		return err
 	}
 
+	fmt.Println("Defer stream.close", err)
 	defer func() {
 		close(exit)
+		fmt.Println("stream.closed", err)
 		stream.Close()
 	}()
 
@@ -42,6 +45,8 @@ func (n *Notification) Stream(ctx context.Context, stream server.Streamer) error
 			}
 		}
 	}
+
+	fmt.Println("Stream before return nil")
 
 	return nil
 }
