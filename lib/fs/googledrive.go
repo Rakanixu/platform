8cb61e381f19c804b48e4b417f1ef6779de918a9
Pablo Aguirre <paguirrerubio@gmail.com>
2016-12-08 17:28:15 +0000

    [Ci skip] Delete file notification

--- lib/fs/googledrive.go
+++ lib/fs/googledrive.go
@@ -11,6 +11,7 @@ import (
 	"github.com/kazoup/platform/lib/file"
 	"github.com/kazoup/platform/lib/globals"
 	"github.com/kazoup/platform/lib/image"
+	notification_proto "github.com/kazoup/platform/notification/srv/proto/notification"
 	"github.com/micro/go-micro/client"
 	"golang.org/x/net/context"
 	"golang.org/x/oauth2"
@@ -136,6 +137,14 @@ func (gfs *GoogleDriveFs) DeleteFile(ctx context.Context, c client.Client, rq fi
 
 	if err := c.Call(ctx, req, rsp); err != nil {
 		return nil, err
+	}
+
+	// Publish notification topic, let client know when to refresh itself
+	if err := c.Publish(globals.NewSystemContext(), c.NewPublication(globals.NotificationTopic, &notification_proto.NotificationMessage{
+		Method: globals.NOTIFY_REFRESH_SEARCH,
+		UserId: gfs.Endpoint.UserId,
+	})); err != nil {
+		log.Print("Publishing (notify file) error %s", err)
 	}
 
 	return &file_proto.DeleteResponse{}, nil
