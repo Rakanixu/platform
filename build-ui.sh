24a01b033fb93c8db358f5b6138333f4f04492cd
Radek Dymacz <radekdymacz@gmail.com>
2016-12-13 16:48:40 +0000

    WIP fix imports

--- build-ui.sh
+++ build-ui.sh
@@ -7,8 +7,8 @@ CWD="$(pwd)"
 # Build UI 
 # cleanup
 rm -rf  ui/web/html
-cd ui/src && npm install && bower install
+cd ui/polymer && npm install && bower install
 npm install polymer-cli
 node_modules/polymer-cli/bin/polymer.js build -v
-cd .. && cp -r src web/html
+cd .. && cp -r polymer/build/bundled web/html
 cd $CWD
