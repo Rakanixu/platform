e735e6f1b31acec9886edbdbe16a64017248349e
Pablo Aguirre <paguirrerubio@gmail.com>
2017-01-20 14:14:06 +0000

    [Ci skip] Search healthchecks

---
+++ lib/healthchecks/search.go
@@ -0,0 +1,56 @@
+package healthchecks
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/kazoup/platform/lib/globals"
+	"github.com/micro/go-micro"
+	"github.com/micro/go-os/monitor"
+	"net/http"
+	"strconv"
+)
+
+func RegisterSearchHealthChecks(srv micro.Service, m monitor.Monitor) {
+	searchSrvHealthCheck(srv, m)
+}
+
+func searchSrvHealthCheck(srv micro.Service, m monitor.Monitor) {
+	url := "https://web.kazoup.io:8082/rpc"
+	body := []byte(`{
+		"service":"` + srv.Server().Options().Name + `",
+		"method":"Search.Health",
+		"request":{}
+	}`)
+	n := fmt.Sprintf("%s.health", srv.Server().Options().Name)
+
+	dshc := m.NewHealthChecker(
+		n,
+		"Checking db-srv health",
+		func() (map[string]string, error) {
+			c := &http.Client{}
+			req, err := http.NewRequest(http.MethodPost, url, bytes.NewBuffer(body))
+			if err != nil {
+				return map[string]string{
+					"info": "Error building request",
+				}, err
+			}
+			req.Header.Set("Authorization", globals.SYSTEM_TOKEN)
+			req.Header.Set("Content-Type", "application/json")
+			rsp, err := c.Do(req)
+			if err != nil {
+				return map[string]string{
+					"info": fmt.Sprintf("POST request with body %s failed: %s", string(body), err),
+				}, err
+			}
+
+			return map[string]string{
+				"info":   "OK",
+				"status": strconv.Itoa(rsp.StatusCode),
+			}, nil
+		},
+	)
+
+	if err := m.Register(dshc); err != nil {
+		fmt.Println("ERROR registering HealthChecker %v", n, err)
+	}
+}
