163cdb06c06d4aa8b317c4ac428ec028f351c03f
Pablo Aguirre <paguirrerubio@gmail.com>
2017-02-08 16:50:18 +0000

    [Ci skip] Content extraction

--- lib/fs/googledrive_helpers.go
+++ lib/fs/googledrive_helpers.go
@@ -6,6 +6,7 @@ import (
 	"github.com/kazoup/platform/lib/file"
 	"github.com/kazoup/platform/lib/globals"
 	"github.com/kazoup/platform/lib/image"
+	"github.com/kazoup/platform/lib/tika"
 	"golang.org/x/net/context"
 	"golang.org/x/oauth2"
 	"google.golang.org/api/drive/v3"
@@ -71,6 +72,10 @@ func (gfs *GoogleDriveFs) pushFilesToChanForPage(files []*drive.File) error {
 				log.Println(err)
 			}
 
+			if err := gfs.enrichFile(f); err != nil {
+				log.Println(err)
+			}
+
 			gfs.FilesChan <- NewFileMsg(f, nil)
 		}
 	}
@@ -110,6 +115,31 @@ func (gfs *GoogleDriveFs) generateThumbnail(f *drive.File, id string) error {
 		if err := ncs.Upload(rd, id); err != nil {
 			return err
 		}
+	}
+
+	return nil
+}
+
+// enrichFile sends the original file to tika and enrich KazoupGoogleFile with Tika interface
+func (gfs *GoogleDriveFs) enrichFile(f *file.KazoupGoogleFile) error {
+	if f.Category == globals.CATEGORY_DOCUMENT {
+		// Download file from GoogleDrive, so connector is globals.GoogleDrive
+		gcs, err := cs.NewCloudStorageFromEndpoint(gfs.Endpoint, globals.GoogleDrive)
+		if err != nil {
+			return err
+		}
+
+		rc, err := gcs.Download(f.Original.Id)
+		if err != nil {
+			return err
+		}
+
+		t, err := tika.ExtractContent(rc)
+		if err != nil {
+			return err
+		}
+
+		f.Content = t.Content()
 	}
 
 	return nil
