27318196d88a6327a5ea043ff15d58ca3e1280f4
Pablo Aguirre <paguirrerubio@gmail.com>
2017-05-08 11:23:11 +0100

    WIP

--- audio/srv/subscriber/subscriber_test.go
+++ audio/srv/subscriber/subscriber_test.go
@@ -43,13 +43,95 @@ func TestTaskHandler_Enrich(t *testing.T) {
 }
 
 func TestTaskHandler_queueListener(t *testing.T) {
+	workers := 3
+	th := &taskHandler{
+		enrichMsgChan: make(chan enrichMsgChan, 1000000),
+		workers:       workers,
+	}
 
+	for i := 0; i < th.workers; i++ {
+		go th.queueListener(i)
+	}
+
+	var queueListenerTestData = []struct {
+		msg enrichMsgChan
+	}{
+		{
+			enrichMsgChan{},
+		},
+		{
+			enrichMsgChan{},
+		},
+		{
+			enrichMsgChan{},
+		},
+	}
+
+	for _, tt := range queueListenerTestData {
+		th.enrichMsgChan <- tt.msg
+	}
+
+	if len(queueListenerTestData) != len(th.enrichMsgChan) {
+		t.Error("Expected %v, got %v", len(queueListenerTestData), len(th.enrichMsgChan))
+	}
 }
 
 func TeststartWorkers(t *testing.T) {
+	workers := 5
+	th := &taskHandler{
+		enrichMsgChan: make(chan enrichMsgChan, 1000000),
+		workers:       workers,
+	}
 
+	var queueListenerTestData = []struct {
+		msg enrichMsgChan
+	}{
+		{
+			enrichMsgChan{},
+		},
+		{
+			enrichMsgChan{},
+		},
+		{
+			enrichMsgChan{},
+		},
+	}
+
+	startWorkers(th)
+
+	for _, tt := range queueListenerTestData {
+		th.enrichMsgChan <- tt.msg
+	}
+
+	if len(queueListenerTestData) != len(th.enrichMsgChan) {
+		t.Error("Expected %v, got %v", len(queueListenerTestData), len(th.enrichMsgChan))
+	}
 }
 
 func TestprocessEnrichMsg(t *testing.T) {
+	var enrichMsgTestData = []struct {
+		msg    enrichMsgChan
+		result error
+	}{
+		{
+			enrichMsgChan{},
+			nil,
+		},
+		/*		{
+					enrichMsgChan{},
+					nil,
+				},
+				{
+					enrichMsgChan{},
+					nil,
+				},*/
+	}
 
+	for _, tt := range enrichMsgTestData {
+		result := processEnrichMsg(tt.msg)
+
+		if tt.result != result {
+			t.Errorf("Expected '%v', got: '%v'", tt.result, result)
+		}
+	}
 }
