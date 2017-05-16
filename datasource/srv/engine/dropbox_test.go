f9dee12b9ca0a07aa863ecbe199725dc833de6d5
Pablo Aguirre <paguirrerubio@gmail.com>
2017-05-16 14:02:37 +0100

    datasource test

---
+++ datasource/srv/engine/dropbox_test.go
@@ -0,0 +1,73 @@
+package engine
+
+import (
+	"github.com/kazoup/platform/datasource/srv/proto/datasource"
+	_ "github.com/kazoup/platform/lib/db/config/mock"
+	_ "github.com/kazoup/platform/lib/db/operations/mock"
+	"github.com/kazoup/platform/lib/globals"
+	"golang.org/x/net/context"
+	"testing"
+)
+
+func TestDropbox_Validate(t *testing.T) {
+	b := Dropbox{
+		Endpoint: proto_datasource.Endpoint{
+			Url: globals.Mock,
+		},
+	}
+
+	endpoint, err := b.Validate(context.TODO(), "[]")
+	if err != nil {
+		t.Fatalf("Unexpected error: %v", err)
+	}
+
+	if len(endpoint.Id) == 0 {
+		t.Error("Id not found")
+	}
+
+	if len(endpoint.Index) == 0 {
+		t.Error("Index not found")
+	}
+}
+
+func TestDropbox_Save(t *testing.T) {
+	b := Dropbox{
+		Endpoint: proto_datasource.Endpoint{
+			Url: globals.Mock,
+		},
+	}
+
+	result := b.Save(context.TODO(), proto_datasource.Endpoint{
+		Url: globals.Mock,
+	}, "test_id")
+
+	if result != nil {
+		t.Errorf("Unexpected error: %v", result)
+	}
+}
+
+func TestDropbox_Delete(t *testing.T) {
+	b := Dropbox{
+		Endpoint: proto_datasource.Endpoint{
+			Url: globals.Mock,
+		},
+	}
+
+	result := b.Delete(context.TODO())
+	if result != nil {
+		t.Errorf("Unexpected error: %v", result)
+	}
+}
+
+func TestDropbox_CreateIndexWithAlias(t *testing.T) {
+	b := Dropbox{
+		Endpoint: proto_datasource.Endpoint{
+			Url: globals.Mock,
+		},
+	}
+
+	result := b.CreateIndexWithAlias(context.TODO())
+	if result != nil {
+		t.Errorf("Unexpected error: %v", result)
+	}
+}
