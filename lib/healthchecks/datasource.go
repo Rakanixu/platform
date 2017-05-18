1607495f89866e2bae2b1cbae30f8c8ab50656e3
Pablo Aguirre <paguirrerubio@gmail.com>
2017-05-18 10:19:40 +0100

    Fix healthchecks

--- lib/healthchecks/datasource.go
+++ lib/healthchecks/datasource.go
@@ -18,7 +18,7 @@ func datasourceSrvHealthCheck(srv micro.Service, m monitor.Monitor) {
 	url := "https://web.kazoup.io:8082/rpc"
 	body := []byte(`{
 		"service":"` + srv.Server().Options().Name + `",
-		"method":"DataSource.Health",
+		"method":"Service.Health",
 		"request":{}
 	}`)
 	n := fmt.Sprintf("%s.health", srv.Server().Options().Name)
